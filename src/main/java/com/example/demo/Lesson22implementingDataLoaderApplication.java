17640691df52e7747cc25e2a1c3cb6dcb6b0baeb
ZoeyHtut <zoeymhtut@yahoo.com>
2018-07-17 15:51:36 -0400

    ImplementingDataLoader

---
+++ src/main/java/com/example/demo/Lesson22implementingDataLoaderApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lesson22implementingDataLoaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lesson22implementingDataLoaderApplication.class, args);
+    }
+}
